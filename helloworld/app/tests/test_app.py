ed68f884d69b36476ebe0fa65b198abc1c257235
Abhishek Saklani <abhishek.saklani@urbanpiper.com>
2020-12-10 12:40:04 +0530

    added pytest

---
+++ helloworld/app/tests/test_app.py
@@ -0,0 +1,75 @@
+import pytest
+from django.test import Client
+import time
+
+class TestApp:
+    def test_1(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+
+    def test_2(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_3(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_4(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_5(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_6(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_7(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_8(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_9(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
+    def test_10(self):
+        '''
+            To test homepage
+        '''
+        time.sleep(5)
+        assert 200 == 200
+    
