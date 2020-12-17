225fc22d2d982ff7ae07a0476560f81f6ec6d508
Abhishek Saklani <abhishek.saklani@urbanpiper.com>
2020-12-17 15:43:16 +0530

    add

--- deploy_commands.sh
+++ deploy_commands.sh
@@ -1,6 +1,7 @@
 echo $0
 CONTAINS_MIGRATION=$1
 echo $CONTAINS_MIGRATION
+git pull origin master
 docker-compose build server
 docker-compose down && docker-compose up -d
 if [ $CONTAINS_MIGRATION == "true" ]
